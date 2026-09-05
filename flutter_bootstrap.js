66fb7933ef6d35462af7c216fab5597e1ba8c6e6
Angel Luis <angelluisbg42@gmail.com>
2026-09-05 12:25:15 -0400

    Deploy: tarjeta de servicio a pantalla completa con foto, degradado y Reservar en texto

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3755261478" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1001094591" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
