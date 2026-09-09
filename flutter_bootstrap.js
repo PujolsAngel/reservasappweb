478f97ff160bc1021290f3687d2fb1c013d67ea1
Angel Luis <angelluisbg42@gmail.com>
2026-09-09 13:53:11 -0400

    Deploy: fondo del perfil de profesional tenido con el color de acento del negocio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1001094591" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "189249203" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
